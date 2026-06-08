a67f1b70c2334068f91f6553fbe26086dbba132b
afurkan55 <afurkanhidir@gmail.com>
2026-06-08 14:07:31 +1000

    Add unit tests

--- TodoListApi.Tests/TodoControllerTests.cs
+++ TodoListApi.Tests/TodoControllerTests.cs
@@ -46,4 +46,28 @@ public class TodoControllerTests
         var todos = result!.Value as List<TodoItem>;
         Assert.Empty(todos!);
     }
+
+    [Fact]
+    public void Update_UpdatesIsCompleted_WhenIdExists()
+    {
+        var controller = CreateController();
+        var item = new TodoItem { Title = "Test todo" };
+        controller.Add(item);
+
+        var updatedItem = new TodoItem { Id = 1, Title = "Test todo", IsCompleted = true };
+        controller.Update(1, updatedItem);
+
+        var result = controller.GetAll() as OkObjectResult;
+        var todos = result!.Value as List<TodoItem>;
+        Assert.True(todos![0].IsCompleted);
+    }
+
+    [Fact]
+    public void Update_ReturnsNotFound_WhenIdDoesNotExist()
+    {
+        var controller = CreateController();
+        var updatedItem = new TodoItem { Id = 99, Title = "Test", IsCompleted = true };
+        var result = controller.Update(99, updatedItem);
+        Assert.IsType<NotFoundResult>(result);
+    }
 }
